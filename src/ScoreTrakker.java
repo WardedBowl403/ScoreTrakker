2c16ea72b9518e8614862b5ef6dc7ab99106103c
Zane Beasecker <zanebeasecker@mymail.mines.edu>
2021-09-27 01:34:31 -0600

    Student class implemented with test files except for processFiles() Driver: Mark Navigator: Zane

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -1,22 +1,50 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.io.*;
 
 public class ScoreTrakker {
 	private ArrayList<Student> students;
 	
-	void loadDataFile(String fileName) {
+	ScoreTrakker() {
+		students = new ArrayList<Student>();
+	}
 	
+	void loadDataFile(String fileName) {
+		try {
+			FileReader reader = new FileReader(fileName);
+			Scanner scanner = new Scanner(reader);
+			int i = 0;
+			while(scanner.hasNext()) {
+				String first = scanner.next();
+				String last = scanner.next();
+				String name = first + " " + last;
+				String s = scanner.next();
+				try {
+					int score = Integer.valueOf(s);
+					students.add(new Student(name, score));
+				} catch (NumberFormatException e) {
+					System.out.println("Incorrect format for "+name+" not a valid score: "+s+"\n");
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("Can't open file: " + fileName);
+		} 
 	}
 	
 	void printInOrder() {
-		
+		System.out.println("Student Scores List");
+		for(Student student : students) {
+			System.out.println(student.toString());
+		}
 	}
 	
 	void processFiles() {
 		
 	}
 	
-	void main(String[] args) {
-		
+	public static void main(String[] args) {
+		ScoreTrakker st = new ScoreTrakker();
+		st.loadDataFile("scores.txt");
+		st.printInOrder();
 	}
 }
