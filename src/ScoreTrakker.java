4be6ccfc1871b435bfaa85ec5e8833d5f27096c6
Zane Beasecker <zanebeasecker@mymail.mines.edu>
2021-09-27 00:31:06 -0600

    Created classes and empty method framework Driver: Mark Navigator: Zane

---
+++ src/ScoreTrakker.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class ScoreTrakker {
+	private ArrayList<Student> students;
+	
+	void loadDataFile(String fileName) {
+		
+	}
+	
+	void printInOrder() {
+		
+	}
+	
+	void processFiles() {
+		
+	}
+	
+	void main(String[] args) {
+		
+	}
+}
---
+++ src/Student.java
@@ -0,0 +1,15 @@
+
+public class Student implements Comparable<Student> {
+	private String name;
+	private int score;
+	
+	@Override
+	public int compareTo(Student o) {
+		return 0;
+	}
+	
+	@Override
+	public String toString() {
+		return "";
+	}
+}
