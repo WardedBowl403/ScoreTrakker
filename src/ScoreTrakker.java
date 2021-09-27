44b9abca58c651cccf5e33997a25aac2be5fc13e
Zane Beasecker <zanebeasecker@mymail.mines.edu>
2021-09-27 01:43:37 -0600

    Implemented processFiles() removed redundant code Driver: Zane Navigator: Mark

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -4,31 +4,28 @@ import java.io.*;
 
 public class ScoreTrakker {
 	private ArrayList<Student> students;
+	private String[] files = {"scores.txt", "badscore.txt", "nofile.txt"};
 	
 	ScoreTrakker() {
 		students = new ArrayList<Student>();
 	}
 	
-	void loadDataFile(String fileName) {
-		try {
-			FileReader reader = new FileReader(fileName);
-			Scanner scanner = new Scanner(reader);
-			int i = 0;
-			while(scanner.hasNext()) {
-				String first = scanner.next();
-				String last = scanner.next();
-				String name = first + " " + last;
-				String s = scanner.next();
-				try {
-					int score = Integer.valueOf(s);
-					students.add(new Student(name, score));
-				} catch (NumberFormatException e) {
-					System.out.println("Incorrect format for "+name+" not a valid score: "+s+"\n");
-				}
+	void loadDataFile(String fileName) throws FileNotFoundException{
+		students = new ArrayList<Student>();
+		FileReader reader = new FileReader(fileName);
+		Scanner scanner = new Scanner(reader);
+		while(scanner.hasNext()) {
+			String first = scanner.next();
+			String last = scanner.next();
+			String name = first + " " + last;
+			String s = scanner.next();
+			try {
+				int score = Integer.parseInt(s);
+				students.add(new Student(name, score));
+			} catch (NumberFormatException e) {
+				System.out.println("Incorrect format for "+name+" not a valid score: "+s+"\n");
 			}
-		} catch (FileNotFoundException e) {
-			System.out.println("Can't open file: " + fileName);
-		} 
+		}
 	}
 	
 	void printInOrder() {
@@ -36,15 +33,22 @@ public class ScoreTrakker {
 		for(Student student : students) {
 			System.out.println(student.toString());
 		}
+		System.out.println("");
 	}
 	
 	void processFiles() {
-		
+		for(String file : files) {
+			try {
+				loadDataFile(file);
+				printInOrder();
+			} catch (FileNotFoundException e) {
+				System.out.println("Can't open file: " + file);
+			}
+		}
 	}
 	
 	public static void main(String[] args) {
 		ScoreTrakker st = new ScoreTrakker();
-		st.loadDataFile("scores.txt");
-		st.printInOrder();
+		st.processFiles();
 	}
 }
